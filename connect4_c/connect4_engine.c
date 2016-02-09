84d39bb0023b4795332001e68881eb0283891ffe
Josiah Campbell <campbjos@mail.gvsu.edu>
2016-02-09 15:16:29 -0500

    Rename connect4 main

--- connect4_c/connect4_engine.c
+++ connect4_c/connect4_engine.c
@@ -2,20 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int current_board_size;
 
 int place_token(int player, int column, int num_rows, 
                         int num_columns, int board[num_rows][num_columns]) {
-    current_board_size = num_rows * num_columns;
-    for (int i = 0; i < current_board_size; i++) {
-        
+    int column_index = num_columns - 1;
+    if (column > column_index || column < 0) {
+        // Don't place token if 
+        return -1;
     }
-}
 
+    return -1;
+}
 
 int winner(int num_rows, int num_columns, int length_to_win, 
         int array[num_rows][num_columns]) {
-    
+   return -1; 
 }
 
+int check_winner_diagonal_ascend(int num_rows, int num_columns, int length_to_win,
+        int array[num_rows][num_columns]) {
+    return -1;
+}
+
+
+int check_winner_diagonal_descend(int num_rows, int num_columns, int length_to_win,
+        int array[num_rows][num_columns]) {
+    return -1;
+}
 
--- connect4_c/connect4_main.c
+++
@@ -1,3 +0,0 @@
-#include "connect4_engine.h"
-
-
