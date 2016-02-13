e72cc83e8ecc5615101631a6dfd2c861fc87f544
Josiah Campbell <josiahcampbell64@gmail.com>
2016-02-13 13:53:50 -0500

    Add main to connect4

--- connect4_c/connect4.c
+++ connect4_c/connect4.c
@@ -1,15 +1,72 @@
 #include "connect4_engine.h"
 #include <stdio.h>
 #include <stdlib.h>
+#define EMPTY -1
+#define VALID_INPUT 1
+
+/* prototypes */
+int print_board(
+      int row_size, int column_size, int board[row_size][column_size]);
+void initialize(int num_rows, int num_cols, int board[num_rows][num_cols]);
 
 int main(int argc, char* argv[3]) {
     if (argc < 3) {
       printf("Not enough inputs!\n");
       return -1;
     }
+    int game_in_session = 1;
     int cell_dimen = atoi(argv[1]);
-    int num_to_win = atoi(argv[2]);
+    int length_to_win = atoi(argv[2]);
+    int player;
+    int column;
+    int board[cell_dimen][cell_dimen];
     printf("Board size: %d x %d, To win: %d\n",
-        cell_dimen, cell_dimen, num_to_win);
+        cell_dimen, cell_dimen, length_to_win);
+    initialize(cell_dimen, cell_dimen, board);
+    do {
+     printf("Ready player: ");
+     if (scanf("%d", &player) != VALID_INPUT) {
+        printf("Invalid player!\n");
+        fpurge(stdin);
+        continue;
+     }
+     printf("Enter column: ");
+     if (scanf("%d", &column) != VALID_INPUT) {
+         printf("Invalid column!\n");
+         fpurge(stdin);
+         continue;
+     }
+     place_token(player, column, cell_dimen, cell_dimen, board);
+     int win = winner(cell_dimen, cell_dimen, length_to_win, board);
+     if (win >= 0) {
+       printf("Win: %d!\n", win);
+     }
+     print_board(cell_dimen, cell_dimen, board);
+    } while(game_in_session);
     return 0;
+}
+
+int print_board(int row_size, int column_size,
+  int board[row_size][column_size]) {
+    for (int i = 0; i < row_size; i++) {
+        for (int j = 0; j < column_size; j++) {
+            if (board[i][j] == -1) {
+              printf("%s  ", ".");
+            } else {
+              printf("%d  ", board[i][j]);
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
+
+void initialize(int num_rows, int num_cols, int board[num_rows][num_cols]) {
+  int r, c;
+  for (r = 0; r < num_rows; r++) {
+    for (c = 0; c < num_cols; c++) {
+      board[r][c] = EMPTY;
+    }
+  }
 }
--- connect4_c/connect4_engine.c
+++ connect4_c/connect4_engine.c
@@ -1,55 +1,65 @@
 #include "connect4_engine.h"
+#include <stdio.h>
 
+/* Non-global prototypes */
+int check_winner_vertical(int num_rows, int num_columns,
+  int length_to_win, int array[num_rows][num_columns]);
+int check_winner_horizontal(int num_rows, int num_columns,
+  int length_to_win, int array[num_rows][num_columns]);
+int check_winner_diagonal_left_down(int num_rows, int num_columns,
+  int length_to_win, int array[num_rows][num_columns]);
+int check_winner_diagonal_right_down(int num_rows, int num_columns,
+  int length_to_win, int array[num_rows][num_columns]);
+int open_space(int column, int num_rows, int num_columns,
+  int array[num_rows][num_columns]);
 /* arrays are pointers! if they're passed, they modify
  * the overall reference */
-
 int place_token(int player, int column, int num_rows,
                         int num_columns, int board[num_rows][num_columns]) {
     int column_index = num_columns - 1;
-    if (column > column_index || column < 0) {
-        // Don't place token if
-        return ;
+    if (column < column_index || column >= 0) {
+        int open_row = open_space(column, num_rows, num_columns, board);
+        if (open_row >= 0) {
+          board[open_row][column] = player;
+          return player; // This player was successfully placed
+        }
     }
     return -1;
 }
 
 int winner(int num_rows, int num_columns, int length_to_win,
         int array[num_rows][num_columns]) {
-          int wins[4] = -1;
+          int wins[4] = {-1};
           int player_one_win = -1;
           int player_two_win = -1;
-          wins[0] = check_winner_vertical(num_rows, num_columns, length_to_win, array);
-          wins[1] = check_winner_horizontal(num_rows, num_columns, length_to_win, array);
+          wins[0] = check_winner_vertical(
+                        num_rows, num_columns, length_to_win, array);
+          wins[1] = check_winner_horizontal(
+                        num_rows, num_columns, length_to_win, array);
           wins[2] = check_winner_diagonal_right_down(
                       num_rows, num_columns, length_to_win, array);
           wins[3] = check_winner_diagonal_left_down(
                       num_rows, num_columns, length_to_win, array);
           for(int i = 0; i < 3; i++) {
-            switch (win[i]) {
-              case 0:
+              if (wins[i] == 0)
                 player_one_win = 1;
-                break;
-              case 1:
+              if (wins[i] == 1)
                 player_two_win = 1;
-                break;
-              default:
-                break;
-            }
           }
           if (player_one_win && player_two_win) {
             return 2;
           } else if (player_one_win) {
             return 0;
           } else if (player_two_win) {
-            return 1
+            return 1;
           }
           return -1;
 }
 
 int check_winner_vertical(int num_rows, int num_columns,
   int length_to_win, int array[num_rows][num_columns]) {
-    int max_col_index = num_columns - 2;
-    int max_row_index = num_rows - 2;
+    int max_col_index = num_columns - 1;
+    int max_row_index = num_rows - 1;
     int win_count = 0;
     int index_player = -1;
 
@@ -73,8 +83,8 @@ int check_winner_vertical(int num_rows, int num_columns,
 
 int check_winner_horizontal(int num_rows, int num_columns,
   int length_to_win, int array[num_rows][num_columns]) {
-    int max_col_index = num_columns - 2;
-    int max_row_index = num_rows - 2;
+    int max_col_index = num_columns - 1;
+    int max_row_index = num_rows - 1;
     int win_count = 0;
     int index_player = -1;
 
@@ -97,7 +107,9 @@ int check_winner_horizontal(int num_rows, int num_columns,
 }
 
 int check_winner_diagonal_right_down(int num_rows, int num_columns,
-  int length_to_win, int array[num_rows][num_columns]) {
+  int length_to_win, int board[num_rows][num_columns]) {
+    int win_count;
+    int diag_max = num_rows;
     for (int diag_right = 2; diag_right < diag_max * 2 - 3; diag_right++) {
         int extra;
         if (diag_right < diag_max) {
@@ -115,22 +127,24 @@ int check_winner_diagonal_right_down(int num_rows, int num_columns,
             if (win_count == 0 && board[row][col] != -1) {
                 ++win_count;
             }
-            printf("%d ", board[row][col]);
             if (board[row - 1][col - 1] == board[row][col] &&
                   board[row][col] != -1) {
               ++win_count;
             } else {
               win_count = 0;
             }
-            if (win_count >= lenght_to_win) {
-                printf("\tWe have a winner of %d!", win_count);
+            if (win_count >= length_to_win) {
+                return board[row][col];
             }
         }
-    return -1;
+  }
+  return -1;
 }
 
 int check_winner_diagonal_left_down(int num_rows, int num_columns,
-  int length_to_win, int array[num_rows][num_columns]) {
+  int length_to_win, int board[num_rows][num_columns]) {
+    int win_count;
+    int diag_max = num_rows;
     for (int diag_left = 2; diag_left < num_rows * 2 - 3; diag_left++) {
         int extra;
         if (diag_left < diag_max) {
@@ -151,10 +165,22 @@ int check_winner_diagonal_left_down(int num_rows, int num_columns,
             } else {
               win_count = 0;
             }
-            if (win_count >= lenght_to_win) {
+            if (win_count >= length_to_win) {
               return board[row][col];
             }
         }
     }
     return -1;
+}
+
+int open_space(int column, int num_rows, int num_columns,
+  int board[num_rows][num_columns]) {
+    int current_index;
+      for (int row = num_rows; row >= 0; --row) {
+         current_index = board[row][column];
+         if (current_index < 0) {
+           return row;
+         }
+  }
+  return -1; // No open spaces were found
 }
