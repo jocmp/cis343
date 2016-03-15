6ad4fff1c89255323f3ad0004d0bea9a3997e3ca
Josiah Campbell <josiahcampbell64@gmail.com>
2016-03-14 20:36:39 -0400

    Add board model

---
+++ project/connect4_python/model/board.py
@@ -0,0 +1,17 @@
+class Board(object):
+
+    def __init__(self, dimension):
+        self.rows = dimension
+        self.columns = dimension
+        self.grid = [[-1 for x in xrange(self.rows)] for y in xrange(self.columns)]
+
+    def __str__(self):
+        print_board = ""
+        for i in xrange(0, self.rows):
+            for j in xrange(0, self.columns):
+                if self.grid[i][j] == -1:
+                    print_board += ".  "
+                else:
+                    print_board += str(self.grid[i][j]) + "  "
+            print_board += "\n"
+        return print_board
