74ebe47ba3a353b0a80c8a1532fbf8496a70754a
Josiah Campbell <josiahcampbell64@gmail.com>
2016-03-21 16:13:57 -0400

    Update main view

--- project/connect4_python/view/mainView.py
+++ project/connect4_python/view/mainView.py
@@ -8,16 +8,24 @@ class MainView:
 
     def __init__(self):
         options, args = getopt.getopt(sys.argv[1:], "")
-        args = [int(x) for x in args]
+        self.engine = None
+        try:
+            args = [int(x) for x in args]
+        except ValueError:
+            print 'Error parsing integer values.'
+            return None
         if len(sys.argv) > 2:
             self.engine = Connect4Engine(args[0], args[1])
+        else:
+            print 'Not enough arguments!'
 
     def run(self):
         print self.engine.board
 
 def main():
     main_view = MainView()
-    main_view.run()
+    if main_view.engine is not None:
+        main_view.run()
 
 if __name__ == '__main__':
     main()
