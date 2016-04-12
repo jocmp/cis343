e825bc7c445602ebf24afec575783ffbbce0a706
Josiah Campbell <josiahcampbell64@gmail.com>
2016-04-12 09:44:58 -0400

    Finish p.o.s. haskell program

--- project/pos_haskell/pos.hs
+++ project/pos_haskell/pos.hs
@@ -2,7 +2,7 @@ import Data.Time.Clock
 import Data.Time.Calendar
 
 -- Define a constant Float called taxRate as 0.065
-taxRate :: Double
+taxRate :: Float
 taxRate = 0.065
 
 digitize :: Integer -> [Integer]
@@ -15,14 +15,27 @@ cardType x | (head (digitize x)) == 4 = "Visa"
            | (head (digitize x)) == 6 = "Discover"
            | otherwise = "Not Accepted"
 
-pastDate :: Integer -> Int -> Int -> Bool
-pastDate cardYear cardMonth cardDay
-    | cardYear > year = True
-    | cardMonth > month = True
-    | cardDay > day = True
-    | otherwise = False
-    where
-      (year, month, day) =  date
+pastDate :: Integer -> Int -> Int -> IO Bool
+pastDate yr mn dy = do
+  currentDay <- fmap utctDay getCurrentTime
+  return((fromGregorian yr mn dy) < currentDay)
 
-date :: IO (Integer, Int, Int) -- :: (year,month,day)
-date = fmap (toGregorian . utctDay) getCurrentTime
+calcTax :: Float -> Float
+calcTax item = item * taxRate
+
+verifyCard :: Integer -> Integer -> Int -> Int -> IO Bool
+verifyCard cardNo year month day = do
+  accepted <- pastDate year month day
+  return (not(accepted) && (cardType cardNo /= "Not Accepted"))
+
+-- user input with MONADS
+checkOut :: Float -> IO ()
+checkOut num = do
+    putStrLn "Enter cost: "
+    cost <- getLine
+    -- reading whatever into a float
+    let amount = (read cost :: Float)
+    if amount /= 0
+      then do putStrLn (show amount)
+              checkOut (amount + num)
+    else putStrLn (show (amount + (num + (calcTax (amount + num)))))
